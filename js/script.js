abf97bbe40704e15099fd4c1db909edc071cd6ea
PowerNether <slavacraft07@gmail.com>
2021-11-21 00:57:21 +0500

    fix

--- js/script.js
+++ js/script.js
@@ -2,15 +2,33 @@ const menu = document.querySelector('.header')
 const menu_toggler = document.querySelector('.header-toggle')
 const menu_overlay = document.querySelector('.header-overlay')
 
+window.onscroll = function () {
+    const body = document.querySelector('body')
+    if (body.classList.contains('front')) {
+        const main_info = document.querySelector('.main').getBoundingClientRect()
+        if (window.pageYOffset >= (main_info.x + main_info.height)) {
+            menu.classList.add('fixed')
+        } else {
+            menu.classList.remove('fixed')
+        }
+    } else {
+        if (window.pageYOffset >= menu.scrollHeight) {
+            menu.classList.add('fixed')
+        } else {
+            menu.classList.remove('fixed')
+        }
+    }
+};
+
 function menu_toggle () {
     if (menu.classList.contains('active')) {
         menu.classList.remove('active')
         menu_overlay.classList.remove('active')
-        document.body.style.overflow = 'auto'
+        document.body.style["overflow-y"] = 'auto'
     } else {
         menu.classList.add('active')
         menu_overlay.classList.add('active')
-        document.body.style.overflow = 'hidden'
+        document.body.style["overflow-y"] = 'hidden'
     }
 }
 
@@ -57,7 +75,7 @@ window.addEventListener('load', function() {
         const top10_swiper = new Swiper(top10_slider, {
             grab: true,
             grabCursor: true,
-            slidesPerView: 2.2,
+            slidesPerView: 1.1,
             spaceBetween: 16,
             scrollbar: {
                 el: '.swiper-scrollbar',
@@ -67,12 +85,77 @@ window.addEventListener('load', function() {
                 prevEl: ".top10-left",
                 nextEl: ".top10-right",
             },
-            // breakpoints: { 
-            //     1264: { 
-            //         slidesPerView: 2.2
-            //     }
-            // }
+            breakpoints: {
+                568: {
+                    slidesPerView: 1.5
+                },
+                768: {
+                    slidesPerView: 2
+                },
+                968: { 
+                    slidesPerView: 2.2
+                }
+            }
         })
         top10_swiper.on('slideChange', () => { top10_current.innerHTML = top10_swiper.realIndex + 1; })    
     }
 })
+
+const main_slider = document.querySelector('.main-right-block__slider')
+
+window.addEventListener('load', function() {
+    if (main_slider !== null) {
+        const main_slider_swiper = new Swiper(main_slider, {
+            grab: true,
+            grabCursor: true,
+            autoplay: {
+                delay: 5000,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+            },
+        })    
+    }
+})
+
+const selects = document.querySelectorAll('.select')
+
+window.addEventListener('load', function () {
+    Array.from(selects).forEach((el) => {
+        $(el).selectric({
+            nativeOnMobile: false
+        });
+        const label = el.parentNode.parentNode.querySelector('.label')
+        const placeholder = el.parentNode.parentNode.querySelector('.placeholder')
+        const select = el.parentNode.parentNode.querySelector('.selectric')
+        const parent = el.parentNode.parentNode.parentNode
+        const amount = parent.querySelector('.amount') || null
+        if (label.textContent === placeholder.textContent) {
+            label.style['color'] = '#808080'
+        }
+        $(el).on('selectric-change', function () {
+            if (label.textContent === placeholder.textContent) {
+                label.style['color'] = '#808080'
+                select.style['border'] = '1px solid #F0F0F0'
+                select.style['background'] = '#FFFFFF'
+                amount.style.opacity = 0
+            } else {
+                if (amount !== null) {
+                    amount.innerHTML = parent.querySelectorAll('.selected').length
+                    amount.style.opacity = 1
+                }
+                label.style['color'] = '#333333'
+                select.style['border'] = '1px solid #00D26D'
+                select.style['background'] = '#F0FAF5'
+            }
+        })
+    })
+})
+
+$("#whatsapp").mask("+7(999) 999-9999", {
+    autoclear: false
+});
+
+$('#phone').mask("+7(999) 999-9999", {
+    autoclear: false
+});
